e585ab9f4ff9a3d9fb4454058651e1c4a0da0f85
Your Name <ueaarthon13117@gmail.com>
2023-11-23 20:29:08 +0700

    finish

---
+++ src/AirPurifier.java
@@ -0,0 +1,55 @@
+//helped by ChatGPT
+import java.util.HashMap;
+
+public class AirPurifier {
+        String model;
+        String serialNo;
+        boolean power;
+        boolean certified = true;
+        int fanspeed;
+
+    static int modelCount = 0;
+    static HashMap<String, Integer> modelOccur = new HashMap<>();
+    void turnOn() {
+        power = true;
+        fanspeed = 1;
+    }
+
+    void turnOff() {
+        power = false;
+        fanspeed = 1;
+    }
+
+    int getFanSpeed() {
+        return fanspeed;
+    }
+    void setFanSpeed(int speed) {
+        fanspeed = Math.min(speed,3);
+    }
+
+
+    AirPurifier(String _model,String _serialNo) {
+        model = _model.toLowerCase();
+        serialNo = _serialNo;
+        modelCount++;
+        modelOccur.put(model, modelOccur.getOrDefault(model, 0) + 1);
+        }
+    static int getModelCount(){
+        return modelCount;
+    }
+    static String mostPopularModel() {
+        String mostPopularModel = null;
+        int maxOccur = 0;
+
+        for (String model : modelOccur.keySet()) {
+            int occur = modelOccur.get(model);
+            if (occur > maxOccur) {
+                maxOccur = occur;
+                mostPopularModel = model;
+            }
+        }
+
+        return mostPopularModel;
+    }
+
+}
